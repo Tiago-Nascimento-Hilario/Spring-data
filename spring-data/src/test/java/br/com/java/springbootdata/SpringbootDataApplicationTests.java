d2a7fbba9f7380cde1844b22ba0c8c26bf1ebfca
Tiago <tnhhilario@gmail.com>
2022-10-19 02:58:30 -0300

    Upload inicial projeto Spring-Data

---
+++ spring-data/src/test/java/br/com/java/springbootdata/SpringbootDataApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.java.springbootdata;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
